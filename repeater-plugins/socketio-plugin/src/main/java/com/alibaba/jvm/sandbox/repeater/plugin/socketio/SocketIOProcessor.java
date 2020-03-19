ec8a68e922c2edbc0a874436cfef5a71052ca32c
xumin <xumin.wlt@gmail.com>
2020-03-19 17:52:40 +0800

    add socketio plugin for netty-socketio repeater

---
+++ repeater-plugins/socketio-plugin/src/main/java/com/alibaba/jvm/sandbox/repeater/plugin/socketio/SocketIOProcessor.java
@@ -0,0 +1,18 @@
+package com.alibaba.jvm.sandbox.repeater.plugin.socketio;
+
+import com.alibaba.jvm.sandbox.repeater.plugin.core.impl.api.DefaultInvocationProcessor;
+import com.alibaba.jvm.sandbox.repeater.plugin.domain.InvokeType;
+
+/**
+ * {@link SocketIOProcessor}
+ * <p>
+ *
+ * @author xuminwlt
+ */
+class SocketIOProcessor extends DefaultInvocationProcessor {
+
+    SocketIOProcessor(InvokeType type) {
+        super(type);
+    }
+
+}
